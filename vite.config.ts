023893da4928b6b6be98cdcdd98fefcdfaba08bc
Mahmoud AboElfadl <iphone011012013@gmail.com>
2025-10-28 02:49:06 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,15 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite'; // لاحظ إننا شيلنا loadEnv
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
     return {
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
       plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
+      // >> الجزء الأخطر (define) اتشال من هنا خلاص <<
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
